ce248a6cc5ae188e1d1c3b3f25bde2c78a84a67b
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-01-10 12:50:26 -0800

    added cred

---
+++ src/cred.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: '73d269dc5bb344b588428ad19485c1b1',
+        ClientSecret: '49b67ae79f8a4412a7b0f324562d014a'
+    }
+}
+
+export { Credentials };
